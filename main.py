dba77dc886e9bf35c7ef6571ee29045c366a38f2
thouay-baccam <thouay.baccam@laplateforme.io>
2024-02-06 14:19:09 +0100

    Fix des noms

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from startpage import StartPage
+from homepage import HomePage
 from loginpage import LoginPage
 from creationpage import CreateAccountPage
 
@@ -16,13 +16,13 @@ class MainApplication(ctk.CTk):
         self.container.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         self.frames = {}
-        for F in (StartPage, LoginPage, CreateAccountPage):
+        for F in (HomePage, LoginPage, CreateAccountPage):
             page_name = F.__name__
             frame = F(parent=self.container, controller=self)
             self.frames[page_name] = frame
             frame.place(relwidth=1, relheight=1)
 
-        self.show_frame("StartPage")
+        self.show_frame("HomePage")
 
     def show_frame(self, page_name):
         frame = self.frames[page_name]
