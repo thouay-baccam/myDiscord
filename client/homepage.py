a461731dc78436384ee6be5240a6bb08a08a706d
thouay-baccam <thouay.baccam@laplateforme.io>
2024-02-06 15:35:06 +0100

    Ajout du backend login aussi

--- client/homepage.py
+++ client/homepage.py
@@ -1,9 +1,10 @@
 import customtkinter as ctk
 
 class HomePage(ctk.CTkFrame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, db_connection=None):
         super().__init__(parent)
         self.controller = controller
+        self.db_connection = db_connection
 
         ctk.CTkLabel(self, text="Welcome to Battias", font=("Arial", 24)).pack(pady=(20, 5))
         ctk.CTkLabel(self, text="We're totally better than Discord", font=("Arial", 14)).pack(pady=(0, 20))
