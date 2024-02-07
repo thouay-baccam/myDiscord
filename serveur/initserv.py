406c828d5cd94ad3e770e6c4cff7c0ba4612630a
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2024-02-07 08:50:09 +0100

    update ip all

--- serveur/initserv.py
+++ serveur/initserv.py
@@ -35,7 +35,7 @@ def handle_client(client, address):
     print(f"Le client d'IP {address} s'est déconnecté")
 
 # Paramètres du serveur
-host = "SENSITIVE_DATA"
+host = "0.0.0.0"
 port = 12345
 
 # Création du socket
