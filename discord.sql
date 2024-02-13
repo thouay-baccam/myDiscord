63222d396f870b330a7147f99f8d0f927ba51b0a
theo-baccam <theo.baccam@laplateforme.io>
2024-02-13 14:42:27 +0100

    feat: status boolean pour account (connexion

--- discord.sql
+++ discord.sql
@@ -25,6 +25,7 @@ CREATE TABLE account (
     name VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    status BOOL DEFAULT 0
 );
 
