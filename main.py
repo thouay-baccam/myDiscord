f1119fc905c271c6b7e7f696cd303c982e681aea
thouay-baccam <thouay.baccam@laplateforme.io>
2024-02-14 14:44:22 +0100

    feat : ajout du prototype fonctionel

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import customtkinter as ctk
 from tkinter import messagebox
 import mysql.connector
-from my_discord.client.homepage import HomePage
-from my_discord.client.loginpage import LoginPage
-from my_discord.client.creationpage import CreateAccountPage
+from StartupPage import StartupPage
+from LoginPage import LoginPage
+from CreateAccountPage import CreateAccountPage
+from MainGUI import MainGUI
 
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("green")
 
 db_connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Teddy2212!", # Modify this for your local setup
+    host="SENSITIVE_DATA",
+    user="user",
+    password="discord",
+    port="SENSITIVE_DATA",
     database="discord"
 )
 
@@ -19,23 +20,35 @@ class MainApplication(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title('Battias')
-        self.geometry('600x600')
 
         self.container = ctk.CTkFrame(self, width=400, height=500)
         self.container.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         self.frames = {}
-        for F in (HomePage, LoginPage, CreateAccountPage):
+        for F in (StartupPage, LoginPage, CreateAccountPage): 
             page_name = F.__name__
             frame = F(parent=self.container, controller=self, db_connection=db_connection)
             self.frames[page_name] = frame
             frame.place(relwidth=1, relheight=1)
 
-        self.show_frame("HomePage")
+        self.show_frame("StartupPage")
 
     def show_frame(self, page_name):
-        frame = self.frames[page_name]
+        if page_name == "MainGUI" and page_name not in self.frames:
+            F = MainGUI 
+            frame = F(parent=self.container, controller=self, db_connection=db_connection)
+            self.frames[page_name] = frame
+            frame.place(relwidth=1, relheight=1)
+        else:
+            frame = self.frames[page_name]
         frame.tkraise()
+
+        if page_name == "MainGUI":
+            self.geometry('1000x600')
+            self.container.configure(width=1000, height=600) 
+        else:
+            self.geometry('600x600') 
+            self.container.configure(width=400, height=500) 
 
 if __name__ == "__main__":
     app = MainApplication()
