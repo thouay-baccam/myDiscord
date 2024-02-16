993b7c358cb5b56a49e6124758dc7e478502a8ea
raphael-attias <raphael.attias@laplateforme.io>
2024-02-16 09:45:34 +0100

    feat SECURITY

--- ChatBackend.py
+++ ChatBackend.py
@@ -2,12 +2,13 @@
 import socket
 import threading
 import datetime
+import config
 
 class ChatBackend:
     def __init__(self, gui, username):
         # Socket connection
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(('SENSITIVE_DATA', 1212))
+        self.client_socket.connect((IP_ADDRESS, PORT))
 
         # Reference to the GUI for updating the textbox
         self.gui = gui
