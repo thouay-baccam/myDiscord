e7ad158cae43d87e6d8993b161b2112476befa0d
theo-baccam <theo.baccam@laplateforme.io>
2024-02-28 09:45:38 +0100

    style: black

--- my_discord/client/main_application.py
+++ my_discord/client/main_application.py
@@ -25,22 +25,24 @@ db_connection = mysql.connector.connect(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     port=os.getenv("DB_PORT"),
-    database=os.getenv("DB_DATABASE")
+    database=os.getenv("DB_DATABASE"),
 )
 
 
 class MainApplication(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title('Battias')
+        self.title("Battias")
 
         self.container = ctk.CTkFrame(self, width=400, height=500)
         self.container.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         self.frames = {}
-        for F in (StartupPage, LoginPage, CreateAccountPage): 
+        for F in (StartupPage, LoginPage, CreateAccountPage):
             page_name = F.__name__
-            frame = F(parent=self.container, controller=self, db_connection=db_connection)
+            frame = F(
+                parent=self.container, controller=self, db_connection=db_connection
+            )
             self.frames[page_name] = frame
             frame.place(relwidth=1, relheight=1)
 
@@ -48,8 +50,10 @@ class MainApplication(ctk.CTk):
 
     def show_frame(self, page_name):
         if page_name == "MainGUI" and page_name not in self.frames:
-            F = MainGUI 
-            frame = F(parent=self.container, controller=self, db_connection=db_connection)
+            F = MainGUI
+            frame = F(
+                parent=self.container, controller=self, db_connection=db_connection
+            )
             self.frames[page_name] = frame
             frame.place(relwidth=1, relheight=1)
         else:
@@ -57,8 +61,8 @@ class MainApplication(ctk.CTk):
         frame.tkraise()
 
         if page_name == "MainGUI":
-            self.geometry('1000x600')
-            self.container.configure(width=1000, height=600) 
+            self.geometry("1000x600")
+            self.container.configure(width=1000, height=600)
         else:
-            self.geometry('600x600') 
-            self.container.configure(width=400, height=500) 
+            self.geometry("600x600")
+            self.container.configure(width=400, height=500)
