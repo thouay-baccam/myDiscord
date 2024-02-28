f40a9cafb1c44d1defc5e67aa44b057443727aa4
theo-baccam <theo.baccam@laplateforme.io>
2024-02-28 10:14:40 +0100

    style(login_page.py): query/messagebox/paramètres en plusieurs lignes

--- my_discord/client/login_page.py
+++ my_discord/client/login_page.py
@@ -20,31 +20,45 @@ class LoginPage(ctk.CTkFrame):
         self.db_connection = db_connection
 
         # Title
-        ctk.CTkLabel(self, text="Login", font=("Arial", 24)).pack(pady=20)
+        ctk.CTkLabel(
+            self,
+            text="Login",
+            font=("Arial", 24)
+        ).pack(pady=20)
 
         # Email Entry
         self.email_label = ctk.CTkLabel(self, text="Email")
         self.email_label.pack(pady=(10, 0))
-        self.email_entry = ctk.CTkEntry(self, width=200, placeholder_text="Email")
+        self.email_entry = ctk.CTkEntry(
+            self,
+            width=200,
+            placeholder_text="Email"
+        )
         self.email_entry.pack()
 
         # Password Entry
         self.password_label = ctk.CTkLabel(self, text="Password")
         self.password_label.pack(pady=(10, 0))
         self.password_entry = ctk.CTkEntry(
-            self, width=200, placeholder_text="Password", show="*"
+            self,
+            width=200,
+            placeholder_text="Password",
+            show="*"
         )
         self.password_entry.pack()
 
         # Login Button
         self.login_button = ctk.CTkButton(
-            self, text="Login", command=self.attempt_login
+            self,
+            text="Login",
+            command=self.attempt_login
         )
         self.login_button.pack(pady=20)
 
         # Back Button
         self.back_button = ctk.CTkButton(
-            self, text="Back", command=lambda: controller.show_frame("StartupPage")
+            self, text="Back",
+            command=lambda: controller.show_frame("StartupPage")
         )
         self.back_button.pack()
 
@@ -57,17 +71,26 @@ class LoginPage(ctk.CTkFrame):
 
         # Query the database to check if the email and hashed password match
         cursor = self.db_connection.cursor()
-        select_query = "SELECT * FROM account WHERE email = %s AND password = %s"
+        select_query = (
+            "SELECT * FROM account "
+            "WHERE email = %s ""AND password = %s"
+        )
         user_data = (email, hashed_password)
         cursor.execute(select_query, user_data)
         result = cursor.fetchone()
         cursor.close()
 
         if result:
-            messagebox.showinfo("Login Successful", "You have been logged in.")
+            messagebox.showinfo(
+                "Login Successful",
+                "You have been logged in."
+            )
             # Store the username in the MainApplication class
             self.controller.username = result[1]
             # Show the MainGUI frame
             self.controller.show_frame("MainGUI")
         else:
-            messagebox.showerror("Login Failed", "Invalid email or password.")
+            messagebox.showerror(
+                "Login Failed",
+                "Invalid email or password."
+            )
